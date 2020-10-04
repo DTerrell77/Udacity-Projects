1b1e97807ad182c0efe0205f6d7bc47f96301627
Dylpickle77 <68970614+Dylpickle77@users.noreply.github.com>
2020-10-04 14:24:29 -0700

    Delete formHandler.test.js

--- src/client/js/formHandler.test.js
+++
@@ -1,6 +0,0 @@
-// Import the js file to test
-import { handleSubmit } from "../src/client/js/formHandler"
-
-test('handleSubmit to be a function', () => {
-  expect(typeof handleSubmit).toBe('function');
-});
